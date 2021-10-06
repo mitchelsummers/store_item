414b706857b87cfeaaf11119355613e1e61a8d29
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-06 13:02:29 -0600

    add inherited class

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   def initialize
     @speed = 0
     @direction = "north"
@@ -15,30 +15,15 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Vehicle
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = "north"
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
   end
